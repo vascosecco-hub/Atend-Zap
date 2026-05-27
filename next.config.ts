ac82f1630d7fda74af7bb5663d1efb9b8f2d55e5
Vasco Secco <vascosecco@gmail.com>
2026-05-27 12:18:23 -0300

    remove problematic config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from 'next'                                                                              
-                  
+import type { NextConfig } from 'next'
+
   const nextConfig: NextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
   }
   export default nextConfig
