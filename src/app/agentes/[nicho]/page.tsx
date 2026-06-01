a43a77413f850941f4e8b32b5d11dc0e395e6de0
Vasco Secco <vascosecco@gmail.com>
2026-06-01 16:38:07 -0300

    fix: iframe height 100% min-height 700px

--- src/app/agentes/[nicho]/page.tsx
+++ src/app/agentes/[nicho]/page.tsx
@@ -105,7 +105,7 @@ export default async function AgentPage({ params }: Props) {
         <iframe
           src={agent.iframeSrc}
           width="100%"
-          style={{ height: '50vh', border: 'none' }}
+          style={{ height: '100%', minHeight: '700px' }}
           allow="microphone;"
           frameBorder="0"
         />
